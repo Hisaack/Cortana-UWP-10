7e5477f220303b83cee1ba59b42db22f136f4b70
AbdiHakim Husssein <40029080+Hisaack@users.noreply.github.com>
2018-11-30 21:54:21 +0300

    Add files via upload
    You can test the Vocal Speech Cortana synthesizer in UWP 10. Create a function with the cortana class that acts as the controller to decide on which perspective can user invoke to respond to your app in actions

---
+++ CortanaFunctions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.UI.Popups;
+
+using Windows.Storage;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.VoiceCommands;
+
+using Windows.System;
+using Windows.Media.SpeechRecognition;
+using Windows.ApplicationModel.Activation;
+
+// for SendSerialData example
+using Windows.Devices.SerialCommunication;
+using Windows.Devices.Enumeration;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml.Controls;
+
+namespace CortanaDemo1
+{
+    class CortanaFunctions
+    {
+        /*
+      This is the lookup of VCD CommandNames as defined in 
+      CustomVoiceCommandDefinitios.xml to their corresponding actions
+      */
+      public void controls()
+        {
+            MainPage x = new MainPage();
+
+            x.webviewXX.Source = new Uri("https://isiolomega.com/Mega/PortalsUI/androidsmartsappsregisterclientsapks.aspx");
+
+        }
+        private readonly static IReadOnlyDictionary<string, Delegate> vcdLookup = new Dictionary<string, Delegate>{
+
+            /*
+            {<command name from VCD>, (Action)(async () => {
+                 <code that runs when that commmand is called>
+            })}
+            */
+           
+
+            {"OpenWebsite", (Action)(async () => {
+
+                //Order Voice to Continue here**
+                MainPage xx=new MainPage();
+
+                //End continue voice**
+                 await WebView.ClearTemporaryWebDataAsync();
+
+                  xx.webviewXX.Source = new Uri("https://isiolomega.com/Mega/PortalsUI/androidsmartsappsregisterclientsapks.aspx");
+
+             })},
+
+            {"OpenFile", (Action)(async () => {
+                StorageFile file = await Package.Current.InstalledLocation.GetFileAsync(@"Test.txt");
+                await Launcher.LaunchFileAsync(file);
+            })},
+
+
+            {"CreateFile", (Action)(async () => {
+                StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+                StorageFile sampleFile = await storageFolder.CreateFileAsync(
+                    @"NewFile.txt", CreationCollisionOption.ReplaceExisting);
+
+                await storageFolder.GetFileAsync("NewFile.txt");
+                await FileIO.WriteTextAsync(sampleFile, "This file was created by Cortana at " + DateTime.Now);
+
+            })},
+
+
+            {"SendSerialData", (Action)(async () => {
+                string comPort = "COM3";
+                string serialMessage = "String sent to the COM port";
+
+                string selector = SerialDevice.GetDeviceSelector(comPort);
+                DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(selector);
+
+                if(devices.Count == 0)
+                {
+                    MessageDialog popup = new MessageDialog($"No {comPort} device found.");
+                    await popup.ShowAsync();
+                    return;
+                }
+
+                DeviceInformation deviceInfo = devices[0];
+                SerialDevice serialDevice = await SerialDevice.FromIdAsync(deviceInfo.Id);
+                serialDevice.BaudRate = 9600;
+                serialDevice.DataBits = 8;
+                serialDevice.StopBits = SerialStopBitCount.Two;
+                serialDevice.Parity = SerialParity.None;
+
+                DataWriter dataWriter = new DataWriter(serialDevice.OutputStream);
+                dataWriter.WriteString(serialMessage);
+                await dataWriter.StoreAsync();
+                dataWriter.DetachStream();
+                dataWriter = null;
+            })},
+
+        };
+
+        /*
+        Register Custom Cortana Commands from VCD file
+        */
+        public static async void RegisterVCD()
+        {
+            StorageFile vcd = await Package.Current.InstalledLocation.GetFileAsync(
+                @"CustomeVoiceCommandDefination.xml");
+
+            await VoiceCommandDefinitionManager
+                .InstallCommandDefinitionsFromStorageFileAsync(vcd);
+        }
+
+        /*
+        Look up the spoken command and execute its corresponding action
+        */
+        public static void RunCommand(VoiceCommandActivatedEventArgs cmd)
+        {
+            SpeechRecognitionResult result = cmd.Result;
+            string commandName = result.RulePath[0];
+            vcdLookup[commandName].DynamicInvoke();
+        }
+    }
+}
